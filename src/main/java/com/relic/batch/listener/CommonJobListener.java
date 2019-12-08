e43238960930716ea5abbabecc06151ce516526d
Relic <592510690@qq,com>
2019-12-08 18:39:22 +0800

    添加监听器耗时统计

--- src/main/java/com/relic/batch/listener/CommonJobListener.java
+++ src/main/java/com/relic/batch/listener/CommonJobListener.java
@@ -6,6 +6,8 @@ import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.listener.JobExecutionListenerSupport;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+
 /**
  * @author Relic
  */
@@ -23,6 +25,8 @@ public class CommonJobListener extends JobExecutionListenerSupport {
     @Override
     public void afterJob(JobExecution jobExecution) {
         log.info("Job [" + jobExecution.getJobId() + "] finish " + DateFormatUtils.format(jobExecution.getEndTime(), DATE_TIME_FORMAT));
+        Date start = jobExecution.getJobParameters().getDate("timestamp");
+        log.info("Job [" + jobExecution.getJobId() + "] spend {} ms", jobExecution.getEndTime().getTime() - start.getTime());
         log.info("Job [" + jobExecution.getJobId() + "] status " + jobExecution.getStatus().name());
     }
 }
